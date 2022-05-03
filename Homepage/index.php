3dbc99d1202967a8b215a7c2ca13aeee6cf41af9
saurya01 <76561844+saurya01@users.noreply.github.com>
2022-05-03 22:35:12 +0530

    Add files via upload

---
+++ Homepage/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv='refresh' content ='3; url=home.php'>
+		</head>
+		<body>
+<div style="
+    height: 200px;
+    width: 200px;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-top: -300px;
+    margin-left: -100px;">
+	<img src="images/CU.jpg"/>
+	<h2> Welcome to University of Calcutta</h2>
+	<h3> Advancing to Placement portal </h3>
+	<h4> Please Wait While we Redirect you to our Page </h4>
+	<h6> Not Redirected?? <a href="home.php">Click Here</a></h6>
+	</div>
+	</body>
+	</html>
